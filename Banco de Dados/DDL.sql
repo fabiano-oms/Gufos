d1975fe5494945a52875ca74bd1d95a35acb25ff
FIC <FIC@info.local>
2019-11-18 11:36:41 -0200

    Estrutura do banco de dados

--- Banco de Dados/DDL.sql
+++ Banco de Dados/DDL.sql
@@ -30,7 +30,8 @@ create table evento(
 	Id_Evento int identity primary key,
 	Titulo_Evento varchar(255) not null,
 	Data_Evento datetime not null,
-	Acesso_Livre bit default(1) not null,
+	Acesso_Livre bit default(1) not null, --The column 'dbo.evento.Acesso_Livre' would normally be mapped to mapped to a non-nullable bool property, but it has a des mapped fault constraint. Such a column is mapped to a nullable bool property to allow a difference between setting the property to false and invoking the default constraint. See https://go.microsoft.com/fwlink/?linkid=851278 for details.
+	-- ******Estamos falando que esta coluna tera por default (1) quando ativa, porém ao mesmo tempo estamos fazendo uma redundancia falando que ela deve ser preenchida.******
 	Id_Categoria int foreign key references categoria(Id_Categoria),
 	Id_Localizacao int foreign key references localizacao(Id_Localizacao)
 );
